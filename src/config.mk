d0705c7fc6f24accbe536432ab0ae7b9e88b772c
Maxim Petrusenko <72452482+MaximPetrusenko@users.noreply.github.com>
2023-02-21 18:59:09 +0100

    Update config.mk

--- src/config.mk
+++ src/config.mk
@@ -1,3 +1,4 @@
 
 
-SOURCES		= v3/src/lift-2D-airfoil-Bernoulli-pressure-coefficients-uncertain.c, v3/src/all_angles.csv
+SOURCES		= v3/src/lift-2D-airfoil-Bernoulli-pressure-coefficients-uncertain.c \
+            v3/src/all_angles.csv
